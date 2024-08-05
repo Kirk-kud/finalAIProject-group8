a11f8370a57230dd94a008f52e9f162a4fd6f0b5
kirk-kudoto <135407806+Kirk-kud@users.noreply.github.com>
2024-08-05 19:31:41 +0000

    Update movie_recommender.py

--- movie_recommender.py
+++ movie_recommender.py
@@ -5,13 +5,13 @@ import pandas as pd
 import gdown
 
 # URL AND GDOWN
-model_url = 'https://drive.google.com/uc?export=download&id=1oxxKFeKbYSqdn0Nk01_Cgdn8GqomRR0R'
+model_url = 'https://drive.google.com/uc?export=download&id=1tIsMTq7T4N3c2PcqYTJKW1h_UWNj26-o'
 gdown.cached_download(model_url, 'kmeans_model.joblib')
 
-vectorizer_url = 'https://drive.google.com/uc?export=download&id=1DDJHjUdMSIhemBTj63IRqh6sDAqAdyyN'
+vectorizer_url = 'https://drive.google.com/uc?export=download&id=15C8_p_rEx4bipwHT-ldWP5ihtT8I90Ys'
 gdown.cached_download(vectorizer_url, 'tfidf_vectorizer.joblib')
 
-df_url = 'https://drive.google.com/uc?export=download&id=1fm8u-eoOuOWIe81iNNjsxLqZ62fAIRFZ'
+df_url = 'https://drive.google.com/uc?export=download&id=1T-OKMXWWjwU12Qjy3V-sPtk2WCbewgVl'
 gdown.cached_download(df_url, 'dataframe.joblib')
 
 # Load the saved models and data
