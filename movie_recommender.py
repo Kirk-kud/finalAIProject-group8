99663d648b2c9e1f5a9b0bd899ad5dbaf71903d5
kirk-kudoto <135407806+Kirk-kud@users.noreply.github.com>
2024-08-05 09:52:53 +0000

    Update movie_recommender.py

--- movie_recommender.py
+++ movie_recommender.py
@@ -5,12 +5,6 @@ import pandas as pd
 import gdown
 
 # URL AND GDOWN
-model_url = 'https://drive.google.com/uc?export=download&id=1uiIjP4Mq6AjQea2-8aOt-jt-VDRrOGep'
-gdown.cached_download(model_url, 'kmeans_model.joblib')
-
-vectorizer_url = 'https://drive.google.com/uc?export=download&id=1ELSwjsET2Mce-ma-B1quuUbg5gtcabtj'
-gdown.cached_download(vectorizer_url, 'tfidf_vectorizer.joblib')
-
 df_url = 'https://drive.google.com/uc?export=download&id=1qe_KZN-0aeYiXD_DLn2V-ajCYpRQU47P'
 gdown.cached_download(df_url, 'dataframe.joblib')
 
